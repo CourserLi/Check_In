e60a4a229b1eefd7140845a502a4d1fece160037
Courser Li <54716852+CourserLi@users.noreply.github.com>
2022-06-02 00:05:14 +0800

    Update healthware.py

--- healthware.py
+++ healthware.py
@@ -16,13 +16,23 @@ def run(playwright: Playwright) -> None:
     page.click("[placeholder=\"密码\"]")
     page.fill("[placeholder=\"密码\"]", password)
     page.click("text=登 录")
+    with context.expect_page() as page_info:
+        page.click("text=健康卡填报"),
+    page1 = page_info.value
+    """
     with page.expect_popup() as popup_info:
         page.click("text=健康卡填报")
     page1 = popup_info.value
+    """
     time.sleep(5)
+    with context.expect_page() as page_info:
+        page1.click("text=我要办理"),
+    page2 = page_info.value
+    """
     with page1.expect_popup() as popup_info:
         page1.click("text=我要办理")
     page2 = popup_info.value
+    """
     time.sleep(5)
     page2.click("text=为了全力做好学校新型冠状病毒感染的肺炎疫情防控工作，我承诺以下内容填写属实。 30s 已阅读并同意 >> ins")
     page2.click("text=下一步")
@@ -34,8 +44,8 @@ def run(playwright: Playwright) -> None:
     page2.click("li[role=\"treeitem\"]:has-text(\"金湾区\")")
     page2.click("input[name=\"jtdz\"]")
     page2.fill("input[name=\"jtdz\"]", "珠海科技学院竹6 501")
-    page2.click("#hsjcsj i").click()
-    page2.click("text=确定").nth(1).click()
+    page2.locator("#hsjcsj i").click()
+    page2.locator("text=确定").nth(1).click()
     page2.click("text=本人承诺登记后、到校前不再前往其他地区 >> ins")
     page2.click("button:has-text(\"提交\")")
     page2.click("a:has-text(\"确定\")")
